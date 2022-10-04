efe7b0cd2ac90cbdab4592b3d50f9496b79ac17e
Jessicat-H <jessica.hannebert@gmail.com>
2022-10-04 14:56:54 -0600

    Library script now exports LD_LIBRARY_PATH

--- library_script.sh
+++ library_script.sh
@@ -4,4 +4,5 @@ gcc -Wall -c ../nic_link/src/nic_lib.c -o nic_lib.o
 mv nic_lib.o src
 cp ../nic_link/src/nic_lib.h src
 cd src
+export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
 gcc -Wall -pthread -o sample j_net.c -lnic_lib -lpigpiod_if2
