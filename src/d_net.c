79d158ba6328583331798b395928f4b4c300f4d4
Dylan Chapell <d_chapell@coloradocollege.edu>
2022-10-04 16:43:34 -0600

    send all queued messages

--- src/d_net.c
+++ src/d_net.c
@@ -1,9 +1,10 @@
-//#include "nic_lib.h"
+#include "nic_lib.h"
 #include <sys/queue.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 struct msgEntry {
+	int port;
 	char* msg;
 	TAILQ_ENTRY(msgEntry) msgEntries;
 };
@@ -17,23 +18,16 @@ TAILQ_HEAD(tailhead, msgEntry);
 
 int main() {
 	//build FIFO Queue to hold messages before they are sent
-	struct msgEntry m1,m2,m3;
-	
 	struct tailhead head;
-
 	TAILQ_INIT(&head);
 
-	m1.msg = "Hello world";
-	m2.msg = "Beaotiful Day Innit!";
-	m3.msg = "Ello Wankas!";
-
-	//insert at tail of queue
-	TAILQ_INSERT_TAIL(&head, &m1, msgEntries);
-	TAILQ_INSERT_TAIL(&head, &m2, msgEntries);
-	TAILQ_INSERT_TAIL(&head, &m3, msgEntries);
-	
-	struct msgEntry *mp;
-	TAILQ_FOREACH(mp, &head, msgEntries)
-		printf("%s\n",mp->msg);
+	while (1) {
+		//if there are messages to be sent, send them (FIFO)
+		if (!TAILQ_EMPTY(&head)) {
+			struct msgEntry *mp;
+			TAILQ_FOREACH(mp, &head, msgEntries)
+				sendMessage(mp->port,mp->msg);
+		}
+	}
 	return 0;
 }
