a69a071c23feae2525803f2c408e7c2d8b683fcf
Dylan Chapell <d_chapell@coloradocollege.edu>
2022-10-04 16:25:25 -0600

    move queue code into a seperate example file

---
+++ src/queue_example.c
@@ -0,0 +1,36 @@
+#include <sys/queue.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct msgEntry {
+	char* msg;
+	TAILQ_ENTRY(msgEntry) msgEntries;
+};
+
+//TODO update accoridng to tony's revision
+void recieveMessage(char* message, int port) {
+	return;
+}
+
+TAILQ_HEAD(tailhead, msgEntry);
+
+int main() {
+	//build FIFO Queue to hold messages before they are sent
+	struct msgEntry m1,m2,m3;
+	struct tailhead head;
+	TAILQ_INIT(&head);
+
+	m1.msg = "Hello world";
+	m2.msg = "Beaotiful Day Innit!";
+	m3.msg = "Ello Wankas!";
+
+	//insert at tail of queue
+	TAILQ_INSERT_TAIL(&head, &m1, msgEntries);
+	TAILQ_INSERT_TAIL(&head, &m2, msgEntries);
+	TAILQ_INSERT_TAIL(&head, &m3, msgEntries);
+	
+	struct msgEntry *mp;
+	TAILQ_FOREACH(mp, &head, msgEntries)
+		printf("%s\n",mp->msg);
+	return 0;
+}
