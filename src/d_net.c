47d1c2f0f0b7ba80adcce33f989467dc97f80f4b
Dylan Chapell <d_chapell@coloradocollege.edu>
2022-10-04 16:12:57 -0600

    Removed malloc calls

--- src/d_net.c
+++ src/d_net.c
@@ -17,27 +17,23 @@ TAILQ_HEAD(tailhead, msgEntry);
 
 int main() {
 	//build FIFO Queue to hold messages before they are sent
-	struct msgEntry *m1,*m2,*m3,*mp;
+	struct msgEntry m1,m2,m3;
+	
 	struct tailhead head;
 
 	TAILQ_INIT(&head);
-	
 
-	m1 = malloc(sizeof(struct msgEntry));
-	m2 = malloc(sizeof(struct msgEntry));
-	m3 = malloc(sizeof(struct msgEntry));
-
-	m1->msg = "Hello world";
-	m2->msg = "Beaotiful Day Innit!";
-	m3->msg = "Ello Wankas!";
+	m1.msg = "Hello world";
+	m2.msg = "Beaotiful Day Innit!";
+	m3.msg = "Ello Wankas!";
 
 	//insert at tail of queue
-	TAILQ_INSERT_TAIL(&head, m1, msgEntries);
-	TAILQ_INSERT_TAIL(&head, m2, msgEntries);
-	TAILQ_INSERT_TAIL(&head, m3, msgEntries);
-
+	TAILQ_INSERT_TAIL(&head, &m1, msgEntries);
+	TAILQ_INSERT_TAIL(&head, &m2, msgEntries);
+	TAILQ_INSERT_TAIL(&head, &m3, msgEntries);
+	
+	struct msgEntry *mp;
 	TAILQ_FOREACH(mp, &head, msgEntries)
 		printf("%s\n",mp->msg);
-    free(m1);free(m2);free(m3);
 	return 0;
 }
