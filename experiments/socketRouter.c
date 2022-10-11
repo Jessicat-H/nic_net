bdf53b206bbc78f59583c24809372fdcbfe92ddb
Dylan Chapell <d_chapell@coloradocollege.edu>
2022-10-11 14:57:04 -0600

    possibly built poll() server

---
+++ experiments/socketRouter.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define PORT_PATH "/tmp/nic_net"
+//not sure what size is ahora
+#define SIZE 123
+#define MAX_CLIENTS 10
+
+/*
+* Code adapted from 
+* https://github.com/weboutin/simple-socket-server/blob/master/poll-server.c
+* 
+*/
+int main(int argc, char** argv) {
+    /* Startup server */
+    int serverSock = socket(AF_LOCAL, SOCK_STREAM, 0);
+
+    struct sockaddr_un addr;
+    addr.sun_family = AF_UNIX;
+    addr.sun_path = PORT_PATH;
+
+    if (bind(serverSock, (struct sockaddr *)&addr, sizeof(addr)) <0) {
+        printf("Error on bind\n");
+        return -1;
+    }
+
+    if (listen(serverSock, 5) < 0) {
+        printf("Error on listen\n");
+        return -1;
+    }
+
+    printf("server started\n");
+
+    /* Server loop */
+
+    struct pollfd pollfds[MAX_CLIENTS + 1];
+    //listen for regular and high priority data
+    pollfds[0].fd = serverSock;
+    pollfds[0].events = POLLIN | POLLPRI;
+    int numClients = 0; //what is this? num clients?
+
+    while (1) {
+        //hang until there is data to read
+        if (poll(pollfds, numClients + 1, -1)<0) {
+            printf("Error on poll\n");
+            return -1;
+        }
+        else {
+            //if there is info on the server's port
+            if (pollfds[0].revents & POLLIN) {
+                struct sockaddr_un cliAddr;
+                int addrLen = sizeof(cliAddr);
+                int clientSock = accept(serverSock, (struct sockaddr *)&cliAddr, &addrLen);
+
+                printf("accept success %s\n", inet_ntoa(cliAddr.sin_addr));
+                //put the client in the list of sockets we r watching
+                for (int i = 1; i < MAX_CLIENTS; i++)
+                {
+                    if (pollfds[i].fd == 0)
+                    {
+
+                        pollfds[i].fd = clientSock;
+                        pollfds[i].events = POLLIN | POLLPRI;
+                        numClients++;
+                        break;
+                    }
+                }
+
+            }
+            //could MAX_CLIENTS instead be useClient?
+            for (int i =1; i < numClients; i++) {
+                if (pollfds[i].fd > 0 && pollfds[i].revents & POLLIN) {
+                    uint8_t buf[SIZE]; //enough for app id + max msg
+                    int bufSize = read(pollfds[i].fd, buf, SIZE - 1);
+                    if (bufSize == -1 || bufSize == 0) {
+                        printf("Read from client failed\n");
+                    }
+                    else {
+                        buf[bufSize] = '\0';
+                        printf("From client: %s\n", buf);
+                    }
+                }
+            }
+
+        }
+    }
+}
