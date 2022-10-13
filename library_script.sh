cc36d82a881109970e7a1bfb890e4d7395c262d6
Dylan Chapell <d_chapell@coloradocollege.edu>
2022-10-13 12:38:58 -0600

    update library script

--- library_script.sh
+++ library_script.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-
 gcc -Wall -c ../nic_link/src/nic_lib.c -o nic_lib.o
 mv nic_lib.o src
 cp ../nic_link/src/nic_lib.h src
 cd src
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
-gcc -Wall -pthread -o nic_net nic_net.c nic_lib.o -lpigpiod_if2 -lrt
+gcc -Wall -pthread -o router router.c nic_net.c nic_lib.o -lpigpiod_if2 -lrt -lpthread
+gcc -Wall -o chat chat.c
